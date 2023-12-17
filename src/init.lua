bc200e378abf561c517d8b619620fa9712c7de0a
cheetiedotpy <77159113+g1mmethemoney@users.noreply.github.com>
2023-12-17 22:06:46 +0300

    Restructure project to model

--- src/init.lua
+++ src/init.lua
@@ -1,2 +1,9 @@
 local Types = require(script.types)
-return require(script.exports) :: Types.WCS
+local TS = require(script.include.RuntimeLib)
+
+local exports = {}
+for _k, _v in TS.import(script, script.Parent, "exports") or {} do
+	exports[_k] = _v
+end
+
+return exports :: Types.WCS
