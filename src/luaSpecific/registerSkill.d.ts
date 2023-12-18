b64c15f377c69651a504cc114d2e6ccb515bf014
cheetiedotpy <77159113+g1mmethemoney@users.noreply.github.com>
2023-12-18 23:03:17 +0300

    Reconfigure for @rbxts/wcs npm package

--- src/luaSpecific/registerSkill.d.ts
+++ src/luaSpecific/registerSkill.d.ts
@@ -1,4 +1,8 @@
 import { Skill } from "source/skill";
 import { Constructor } from "source/utility";
 
+/**
+ * @internal Reserved for LuaU usage
+ * @hidden
+ */
 export declare function RegisterSkill(Name: string): Constructor<Skill>;
--- src/luaSpecific/registerStatusEffect.d.ts
+++ src/luaSpecific/registerStatusEffect.d.ts
@@ -1,4 +1,8 @@
 import { StatusEffect } from "exports";
 import { Constructor } from "source/utility";
 
+/**
+ * @internal Reserved for LuaU usage
+ * @hidden
+ */
 export declare function RegisterStatusEffect(Name: string): Constructor<StatusEffect>;
