f28fb80b1cd4ec7b9a9df0974a6851a006271871
cheetiedotpy <77159113+g1mmethemoney@users.noreply.github.com>
2023-12-19 00:21:04 +0300

    Fix init.lua for roblox-ts usage

--- src/init.lua
+++ src/init.lua
@@ -1,9 +1,2 @@
 local Types = require(script.types)
-local TS = require(script.include.RuntimeLib)
-
-local exports = {}
-for _k, _v in TS.import(script, script, "exports") or {} do
-	exports[_k] = _v
-end
-
-return exports :: Types.WCS
+return require(script.exports) :: Types.WCS
