4ed0522e3374ab6c2e3b6f55c25222fa91e9f67f
cheetiedotpy <77159113+g1mmethemoney@users.noreply.github.com>
2024-01-05 22:28:31 +0300

    feat: make createMoveset register a moveset with a name

--- src/source/moveset.ts
+++ src/source/moveset.ts
@@ -1,16 +1,28 @@
 import { Skill } from "./skill";
-import { Constructor } from "./utility";
+import { Constructor, logError } from "./utility";
 
 export interface Moveset {
     readonly Name: string;
     readonly Skills: Constructor<Skill>[];
 }
 
-export function CreateMoveset(Name: string, Skills: Constructor<Skill>[]) {
+const registeredMovesets = new Map<string, Moveset>();
+
+/**
+ * Creates a moveset with the given name and skills.
+ */
+export function CreateMoveset(Name: string, Skills: Constructor<Skill>[]): Moveset {
+    if (registeredMovesets.has(Name)) {
+        logError(`StatusEffect with name ${Name} was already registered before`);
+    }
+
     const moveset = {
         Name: Name,
         Skills: Skills,
     } as const;
+
+    setmetatable(moveset, { __tostring: () => Name });
     table.freeze(moveset);
+
     return moveset;
 }
